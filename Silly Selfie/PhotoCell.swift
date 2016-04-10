2033faece3999c5b5ee354b8ae4712679ffe9d6f
Kyle Lee <kiloloco@me.com>
2016-04-09 22:29:11 -0700

    basic layout completed

---
+++ Silly Selfie/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  Silly Selfie
+//
+//  Created by Kyle Lee on 4/9/16.
+//  Copyright © 2016 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var selfieImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+}
